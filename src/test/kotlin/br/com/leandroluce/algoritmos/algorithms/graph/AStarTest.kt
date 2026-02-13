c8f148474aeb9499d5debd3d5a09d011847f444e
kryst <krystianyago@gmail.com>
2026-02-13 19:17:52 -0300

    feat: Implement A* algorithm

---
+++ src/test/kotlin/br/com/leandroluce/algoritmos/algorithms/graph/AStarTest.kt
@@ -0,0 +1,32 @@
+package br.com.leandroluce.algoritmos.algorithms.graph
+
+import br.com.leandroluce.algoritmos.datastructures.graph.AdjacencyList
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AStarTest {
+
+    @Test
+    fun `aStar should return the shortest path from a source vertex to a destination`() {
+        val graph = AdjacencyList<String>()
+        val a = graph.createVertex("A")
+        val b = graph.createVertex("B")
+        val c = graph.createVertex("C")
+        val d = graph.createVertex("D")
+        val e = graph.createVertex("E")
+
+        graph.addDirectedEdge(a, b, 2.0)
+        graph.addDirectedEdge(a, c, 5.0)
+        graph.addDirectedEdge(b, d, 4.0)
+        graph.addDirectedEdge(b, e, 1.0)
+        graph.addDirectedEdge(c, e, 3.0)
+        graph.addDirectedEdge(d, e, 1.0)
+
+        val aStar = AStar(graph)
+        val costs = aStar.shortestPath(a, e)
+
+        assertEquals(0.0, costs[a])
+        assertEquals(2.0, costs[b])
+        assertEquals(3.0, costs[e])
+    }
+}
