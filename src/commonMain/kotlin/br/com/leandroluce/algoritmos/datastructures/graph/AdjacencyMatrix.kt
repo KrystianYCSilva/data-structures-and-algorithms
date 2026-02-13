c73daefcf5d936fd68f2d37da566922ff45b82ef
kryst <krystianyago@gmail.com>
2026-02-13 18:48:36 -0300

    feat: Implement AdjacencyMatrix data structure

---
+++ src/commonMain/kotlin/br/com/leandroluce/algoritmos/datastructures/graph/AdjacencyMatrix.kt
@@ -0,0 +1,61 @@
+package br.com.leandroluce.algoritmos.datastructures.graph
+
+class AdjacencyMatrix<T> : Graph<T> {
+
+    private val vertices = arrayListOf<Vertex<T>>()
+    private val weights = arrayListOf<ArrayList<Double?>>()
+
+    override fun createVertex(data: T): Vertex<T> {
+        val vertex = Vertex(vertices.count(), data)
+        vertices.add(vertex)
+        weights.forEach {
+            it.add(null)
+        }
+        val row = ArrayList<Double?>(vertices.count())
+        repeat(vertices.count()) {
+            row.add(null)
+        }
+        weights.add(row)
+        return vertex
+    }
+
+    override fun addDirectedEdge(source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
+        weights[source.index][destination.index] = weight
+    }
+
+    override fun addUndirectedEdge(source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
+        addDirectedEdge(source, destination, weight)
+        addDirectedEdge(destination, source, weight)
+    }
+
+    override fun add(edge: Edge<T>) {
+        when (edge.weight) {
+            null -> addUndirectedEdge(edge.source, edge.destination, null)
+            else -> addDirectedEdge(edge.source, edge.destination, edge.weight)
+        }
+    }
+
+    override fun edges(source: Vertex<T>): ArrayList<Edge<T>> {
+        val edges = arrayListOf<Edge<T>>()
+        (0 until weights.size).forEach { column ->
+            val weight = weights[source.index][column]
+            if (weight != null) {
+                edges.add(Edge(source, vertices[column], weight))
+            }
+        }
+        return edges
+    }
+
+    override fun weight(source: Vertex<T>, destination: Vertex<T>): Double? {
+        return weights[source.index][destination.index]
+    }
+
+    override fun toString(): String {
+        return buildString {
+            vertices.forEach { vertex ->
+                val edgeString = edges(vertex).joinToString { it.destination.data.toString() }
+                append("${vertex.data} -> [$edgeString]\\n")
+            }
+        }
+    }
+}
