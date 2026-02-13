df807fa08f95f724736c26c9d3e0ac9934664953
kryst <krystianyago@gmail.com>
2026-02-13 19:43:02 -0300

    feat: Implement Bubble Sort algorithm

---
+++ src/commonMain/kotlin/br/com/leandroluce/algoritmos/algorithms/sorting/BubbleSort.kt
@@ -0,0 +1,14 @@
+package br.com.leandroluce.algoritmos.algorithms.sorting
+
+fun <T : Comparable<T>> bubbleSort(list: MutableList<T>) {
+    val n = list.size
+    for (i in 0 until n - 1) {
+        for (j in 0 until n - i - 1) {
+            if (list[j] > list[j + 1]) {
+                val temp = list[j]
+                list[j] = list[j + 1]
+                list[j + 1] = temp
+            }
+        }
+    }
+}
